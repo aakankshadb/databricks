ba5d9c4375206a4825ecdfab9497b50cdcf3160f
aakankshadb <132448359+aakankshadb@users.noreply.github.com>
2023-07-05 20:50:49 +0530

    This file contains code for passing parameters values to a notebook from another notebook

---
+++ notebooks/dbutils Notebooks-4.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+dbutils.notebook.run("/Users/aakanksha.shrivas@diggibyte.com/Databricks_commands_Notebooks/dbutils Notebooks -3",180,{"Paracombobox":"combobox","Paramultiselect":"multiselect","Paradropdown":"dropdown","Paratext":"textbox"})
+
+# COMMAND ----------
+
